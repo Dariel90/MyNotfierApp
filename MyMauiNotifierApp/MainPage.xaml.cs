667a92d4c076e219ca7a12b5aaf7c61afd966dd7
Dariel Amores <dariel901231@gmail.com>
2026-02-15 23:49:47 +0100

    Add project files.

---
+++ MyMauiNotifierApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMauiNotifierApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
